04dc247abab6af7048dce0be33f87b453508834c
vitinjesus <vitor.jesus.oliveira12@escola.pr.gov.br>
2026-06-12 10:51:59 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+    'Cachorrinhos',
+    'Qual é a raça de cachorro mais pequena do mundo',
+    'O Chihuahua.'
+)
+
+criaCartao(
+    'Cachorrinhos',
+    'Qual é a raça de cachorro considerada a mais inteligente?',
+    'O Border Collie.'
+)
+
+criaCartao(
+    'Cachorrinhos',
+    'Qual raça é famosa por ser salva-vidas nas montanhas suíças?',
+    'O São Bernardo.'
+)
+
+criaCartao(
+    'Cachorrinhos',
+    'Qual cachorro é conhecido como “o gigante gentil”?',
+    'O Dogue Alemão.'
+)
+
+criaCartao(
+    'Cachorrinhos',
+    'Qual raça foi criada para caçar ratos em minas e fábricas?',
+    'O Terrier.'
+)
+
+criaCartao(
+    'Cachorrinhos',
+    'Qual era a raça preferida da Rainha Elizabeth II?',
+    'O Corgi.'
+)
+
+criaCartao(
+    'Cachorrinhos',
+    'Qual raça japonesa é símbolo de lealdade e inspirou o filme “Hachiko”?',
+    'O Akita Inu.'
+)
+
+criaCartao(
+    'Cachorrinhos',
+    'Qual cachorro é famoso por suas dobras na pele e rosto enrugado?',
+    'O Shar Pei.'
+)
+
+criaCartao(
+    'Cachorrinhos',
+    'Qual raça é conhecida por ter a língua azulada?',
+    'O Chow Chow.'
+)
+
+criaCartao(
+    'Cachorrinhos',
+    'Qual raça tem fama de ser “cão de guarda brasileiro”?',
+    'O Fila Brasileiro.'
+)
+
+criaCartao(
+    'Cachorrinhos',
+    'Qual cachorro é usado como guia para pessoas cegas em muitos países?',
+    'O Labrador Retriever..'
+)
+
+criaCartao(
+    'Cachorrinhos',
+    'Qual é a raça com olfato mais poderoso, usada em resgates e farejamentos?',
+    'O Bloodhound.'
+)
