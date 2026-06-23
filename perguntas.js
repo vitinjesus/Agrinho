da0b50954cfb6c01555a71586faf87d74e837256
vitinjesus <vitor.jesus.oliveira12@escola.pr.gov.br>
2026-06-23 12:26:06 -0300

    ..

--- perguntas.js
+++ perguntas.js
@@ -1,71 +1,70 @@
 criaCartao(
-    'Cachorrinhos',
-    'Qual é a raça de cachorro mais pequena do mundo',
+    'O que é o Programa Agrinho 2026?',
+    'É um programa educacional que promove temas ligados ao meio ambiente, cidadania, saúde e agricultura nas escolas.',
     'O Chihuahua.'
 )
 
 criaCartao(
-    'Cachorrinhos',
-    'Qual é a raça de cachorro considerada a mais inteligente?',
+    'é o tema central do Agrinho 2026?',
+    'O tema pode variar por edição, mas geralmente envolve sustentabilidade, meio ambiente e desenvolvimento rural.',
     'O Border Collie.'
 )
 
 criaCartao(
-    'Cachorrinhos',
-    'Qual raça é famosa por ser salva-vidas nas montanhas suíças?',
+    'Quem organiza o concurso Agrinho no Paraná?',
+    'O programa é organizado pelo Sistema FAEP/SENAR-PR.?',
     'O São Bernardo.'
 )
 
 criaCartao(
-    'Cachorrinhos',
-    'Qual cachorro é conhecido como “o gigante gentil”?',
+    'Quais alunos podem participar do Agrinho 2026?'
+    'Alunos da rede pública e privada do ensino fundamental e médio do Paraná.?',
     'O Dogue Alemão.'
 )
 
 criaCartao(
-    'Cachorrinhos',
-    'Qual raça foi criada para caçar ratos em minas e fábricas?',
-    'O Terrier.'
+    'O que os estudantes devem desenvolver na categoria de Redação?',
+    'Textos relacionados ao tema do Agrinho, mostrando criatividade e conhecimento.'
 )
 
 criaCartao(
-    'Cachorrinhos',
-    'Qual era a raça preferida da Rainha Elizabeth II?',
+    'Qual é o objetivo da categoria de Robótica no Agrinho 2026?',
+    'Incentivar o uso da tecnologia para resolver problemas e desenvolver soluções inovadoras.',
     'O Corgi.'
 )
 
 criaCartao(
-    'Cachorrinhos',
-    'Qual raça japonesa é símbolo de lealdade e inspirou o filme “Hachiko”?',
+    'Quantos integrantes podem formar um grupo nas categorias técnicas?',
+    'Geralmente entre 2 a 5 alunos, dependendo da modalidade.',
     'O Akita Inu.'
 )
 
 criaCartao(
-    'Cachorrinhos',
-    'Qual cachorro é famoso por suas dobras na pele e rosto enrugado?',
+    'Que tipo de premiação os vencedores do Agrinho 2026 recebem?',
+    'Prêmios como computadores, tablets, certificados e reconhecimento escolar.?',
     'O Shar Pei.'
 )
 
 criaCartao(
-    'Cachorrinhos',
-    'Qual raça é conhecida por ter a língua azulada?',
+    'Qual é a importância do tema “Agro forte, futuro sustentável”?',
+    'Ele destaca a importância da agricultura aliada à preservação ambiental e ao desenvolvimento sustentável.?',
     'O Chow Chow.'
 )
 
 criaCartao(
-    'Cachorrinhos',
-    'Qual raça tem fama de ser “cão de guarda brasileiro”?',
+    'Em que período geralmente acontecem as etapas do concurso Agrinho?',
+    'Ao longo do ano letivo, com fases escolares, regionais e estaduais.',
     'O Fila Brasileiro.'
 )
 
 criaCartao(
-    'Cachorrinhos',
-    'Qual cachorro é usado como guia para pessoas cegas em muitos países?',
+    'Como o Agrinho contribui para a educação dos estudantes do Paraná?',
+    'Ele incentiva a aprendizagem prática, consciência ambiental e formação cidadã.',
     'O Labrador Retriever..'
 )
 
 criaCartao(
-    'Cachorrinhos',
-    'Qual é a raça com olfato mais poderoso, usada em resgates e farejamentos?',
+    'Qual é a importância do Agrinho para a comunidade escolar?',
+    'Qual é a raça com olfato mais poderoso, usada em resgates e farejamentos?Ele incentiva projetos educativos que aproximam a escola da realidade do campo e da sociedade, promovendo conhecimento, cidadania e consciência ambiental.',
     'O Bloodhound.'
 )
